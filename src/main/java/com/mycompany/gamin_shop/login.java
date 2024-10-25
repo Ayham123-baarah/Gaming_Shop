2d3254459aa522412e7c3dce546c106b19e41198
ayhamthiab <s12218365@stu.najah.edu>
2024-10-26 00:08:28 +0300

    6th

--- src/main/java/com/mycompany/gamin_shop/login.java
+++ src/main/java/com/mycompany/gamin_shop/login.java
@@ -146,6 +146,7 @@ public class login extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new login().setVisible(true);
+                 System.out.println("meeee");
             }
         });
     }
