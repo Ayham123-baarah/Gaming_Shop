d96da1a2095faf3564e696d01d48e8ec9d1ceef2
Ayham123-baarah <s12218367@stu.najah.edu>
2024-10-25 23:48:38 +0300

    aaaaa

--- src/main/java/com/mycompany/gamin_shop/Gaming_Shop.java
+++ src/main/java/com/mycompany/gamin_shop/Gaming_Shop.java
@@ -14,6 +14,7 @@ public class Gaming_Shop {
               // new login().setVisible(true);
         new main().setVisible(true);
         new main().setVisible(true);
+        new main().setVisible(true);
 
     }
 }
